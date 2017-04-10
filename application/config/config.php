a1d76a6566595c1b28d51e3345ad66405f3b38c6
Jean-Laptop <jeanscastro7@gmail.com>
2017-04-09 20:41:29 -0500

    Commit 2 ubuntu
    Signed-off-by: Jean-Laptop <jeanscastro7@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -23,13 +23,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/EbenezerApp/';
+$config['base_url'] = 'http://localhost/EbenezerApp/';
 
 /*
 |--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
 |
+
+
+
+
 | Typically this will be your index.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
 | variable so that it is blank.
@@ -213,7 +217,7 @@ $config['directory_trigger'] = 'd';
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
